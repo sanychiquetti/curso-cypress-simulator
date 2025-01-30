764d0f97e8dc94eba5928f80196a62a3e16f717f
sanychiquetti <sanymara77@hotmail.com>
2025-01-30 18:42:57 -0300

    Implementando novos teste principais

--- cypress/e2e/cypress-simulator.cy.js
+++ cypress/e2e/cypress-simulator.cy.js
@@ -8,7 +8,7 @@ describe("Cypress Simulator", () => {
     })
     cy.contains("button", "Login").click()
   })
-  it.only("simulation ", () => {
+  it("successfully simulates a Cypress command cy.visit()", () => {
     cy.get("#codeInput").type("cy.visit('https://www.linkedin.com/in/sanychiquettigarcia/')")
     cy.contains("button", "Run").click()
 
@@ -18,24 +18,65 @@ describe("Cypress Simulator", () => {
       .and("be.visible")
   })
 
-  it("error: invalid cypress command", () => {
-    
+  it("shows error message when typing and running invalid Cypress command cy.run()", () => {
+    cy.get("#codeInput").type("cy.run()")
+    cy.contains("button", "Run").click()
+
+    cy.get("#outputArea", { timeout: 6000 })
+      .should("contain", "Error:")
+      .and("contain", "Invalid Cypress command: cy.run()")
+      .and("be.visible")
   })
 
-  it("warning", () => {
-    
+  it("shows warning message when typing and running a not-implemented Cypress command ", () => {
+    cy.get("#codeInput").type("cy.contains('Logout')")
+    cy.contains("button", "Run").click()
+
+    cy.get("#outputArea", { timeout: 6000 })
+      .should("contain", "Warning:")
+      .and("contain", "The `cy.contains` command has not been implemented yet.")
+      .and("be.visible")
   })
 
-  it("error: valid command without parentheses", () => {
-    
+  it("shows error message when typing and running a valid command without parentheses", () => {
+    cy.get("#codeInput").type("cy.visit")
+    cy.contains("button", "Run").click()
+
+    cy.get("#outputArea", { timeout: 6000 })
+      .should("contain", "Error:")
+      .and("contain", "Missing parentheses on `cy.visit` command")
+      .and("be.visible")
   })
 
-  it("help", () => {
-    
+  it("asks for help and gets common Cypress commands and examples with a link to the docs", () => {
+    cy.get("#codeInput").type("help")
+    cy.contains("button", "Run").click()
+
+    cy.get("#outputArea", { timeout: 6000 })
+      .should("contain", "cy.visit(url: string)")
+      .and("contain", "For more commands and details, visit the official Cypress API documentation")
+      .and("be.visible")
+    cy.contains("#outputArea a", "official Cypress API documentation")
+      .should("have.attr", "href", "https://docs.cypress.io/api/table-of-contents")
+      .and("have.attr", "target", "_blank")
+      .and("have.attr", "rel", "noopener noreferrer")
+      .and("be.visible")
   })
 
-  it("maximize/minimize", () => {
-    
+  it.only("maximizes and minimizes a simulation result", () => {
+    cy.get("#codeInput").type("cy.log('Hi there!')")
+    cy.contains("button", "Run").click()
+
+    cy.get(".expand-collapse").click()
+
+    cy.get("#outputArea", { timeout: 6000 })
+      .should("contain", "Success:")
+      .and("contain", "cy.log('Hi there!') // Logged message 'Hi there!'")
+      .and("be.visible")
+    cy.get("#collapseIcon").should("be.visible")
+
+    cy.get(".expand-collapse").click()
+    cy.get("#expandIcon").should("be.visible")
   })
 
   it("logout", () => {
