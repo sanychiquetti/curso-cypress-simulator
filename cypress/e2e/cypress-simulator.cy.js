6d2ff7c3b0d42c94754401f033e31f7c35668e7c
Sany Chiquetti Garcia <sanymara77@hotmail.com>
2025-03-01 10:56:32 -0300

    adicionando testes de loing e logot e local storage

--- cypress/e2e/cypress-simulator.cy.js
+++ cypress/e2e/cypress-simulator.cy.js
@@ -63,7 +63,7 @@ describe("Cypress Simulator", () => {
       .and("be.visible")
   })
 
-  it.only("maximizes and minimizes a simulation result", () => {
+  it("maximizes and minimizes a simulation result", () => {
     cy.get("#codeInput").type("cy.log('Hi there!')")
     cy.contains("button", "Run").click()
 
@@ -79,55 +79,192 @@ describe("Cypress Simulator", () => {
     cy.get("#expandIcon").should("be.visible")
   })
 
-  it("logout", () => {
-    
+  it("logs out successfully", () => {
+    cy.get('#sandwich-menu').click()
+    cy.contains("button", "Logout").click()
+
+    cy.contains("button", "Login").should("be.visible")
+    cy.get('#sandwich-menu').should('not.be.visible')
   })
 
-  it("show and hide button", () => {
-    
+  it("shows and hides the logout  button", () => {
+    cy.get('#sandwich-menu').click()
+
+    cy.contains("button", "Logout").should("be.visible")
+
+    cy.get('#sandwich-menu').click()
+
+    cy.contains("button", "Logout").should("not.be.visible")
   })
 
-  it("running... state", () => {
-    
+  it("shows the running state before showing the final result", () => {
+    cy.get('textarea[placeholder="Write your Cypress code here..."]')
+      .type('cy.log("Hi there!")')
+
+    cy.contains("button", "Run").click()
+
+    cy.contains('button', 'Running...').should('be.visible')
+    cy.contains('#outputArea', 'Running... Please wait.')
+      .should('be.visible')
+
+    cy.contains(
+      'button', 
+      'Running...', 
+      { timeout: 6000}
+    ).should('not.exist')
+    cy.contains('button', 'Run').should('be.visible')
+    cy.contains(
+      '#outputArea', 
+      'Running... Please wait.',
+      { timeout: 6000}
+    ).should('not.exist')
+
+    cy.get('#outputArea')
+      .should('contain', 'Success:')
+      .and('contain', 'cy.log("Hi there!") // Logged message "Hi there!"')
+      .and('be.visible')
   })
 
-  it("accept cookies", () => {
+  it("check the run button disabled/enabled states", () => {
+    cy.contains('button', 'Run')
+      .should('be.disabled')
+
+    cy.get('textarea[placeholder="Write your Cypress code here..."]')
+      .type('cy.log("Hi there!")')
+
+    cy.contains('button', 'Run')
+      .should('be.enabled')
+
+    cy.get('textarea[placeholder="Write your Cypress code here..."]')
+      .clear()
     
+    cy.contains('button', 'Run')
+      .should('be.disabled')
+  })
+
+  it("clear the code input when logging off then logging in again", () => {
+    cy.get('textarea[placeholder="Write your Cypress code here..."]')
+      .type('cy.log("Hi there!")')
+
+    cy.get('#sandwich-menu').click()
+    cy.contains('button', 'Logout').click()
+    cy.contains('button', 'Login').click()
+
+    cy.get('textarea[placeholder="Write your Cypress code here..."]')
+      .should('have.value', '')
+  })
+
+  it("disabled the run button when logging off then logging in again", () => {
+    cy.get('textarea[placeholder="Write your Cypress code here..."]')
+    .type('cy.log("Hi there!")')
+
+   cy.get('#sandwich-menu').click()
+   cy.contains('button', 'Logout').click()
+   cy.contains('button', 'Login').click()
+
+    cy.contains('button', 'Run')
+      .should('be.disabled')
+  })
+
+  it("clears the code output when logging off then logging in again", () => {
+    cy.get('textarea[placeholder="Write your Cypress code here..."]')
+     .type('cy.log("Hi there!")')
+    cy.contains('button', 'Run').click()
+
+    cy.get('#outputArea', { timeout: 6000 })
+      .should('contain', 'Success:')
+      .and('contain', 'cy.log("Hi there!") // Logged message "Hi there!"')
+      .and('be.visible')
+
+    cy.get('#sandwich-menu').click()
+    cy.contains('button', 'Logout').click()
+    cy.contains('button', 'Login').click()
+      
+    cy.get('#outputArea')
+      .and('not.contain', 'cy.log("Hi there!") // Logged message "Hi there!"')
+  })
+
+  it('doesm´t shows the cookie consent banner on the login page', () => {
+    cy.clearAllLocalStorage()
+
+    cy.reload()
+
+    cy.contains('button', 'Login')
+      .should('be.visible')
+    cy.get('#cookieConsent')
+      .should('not.be.visible')
+  })
+})
+
+describe('Cypress Simulator - Cookies consent', () => {
+  beforeEach(() => {
+    cy.visit('./src/index.html?skipCaptcha=true')
+    cy.contains('button', 'Login').click()
+  })
+  it("it consents on the cookies usage", () => {
+    cy.get('#cookieConsent')
+      .as('cookiesConsentBanner')
+      .find('button:contains("Accept")')
+      .click()
+
+    cy.get('@cookiesConsentBanner')
+      .should('not.be.visible')
+    
+      cy.window()
+        .its('localStorage.cookieConsent')
+        .should('be.equal', 'accepted')
   })
 
   it("decline cookies", () => {
-    
-  })
+    cy.get('#cookieConsent')
+      .as('cookiesConsentBanner')
+      .find('button:contains("Decline")')
+      .click()
 
-  it("no cookies banner on the login page", () => {
+    cy.get('@cookiesConsentBanner')
+      .should('not.be.visible')
     
+      cy.window()
+        .its('localStorage.cookieConsent')
+        .should('be.equal', 'declined')
   })
+})
 
-  it("captcha button states", () => {
+describe('Cypress Simulator - Captcha', () => {
+  beforeEach(() => {
+    cy.visit('./src/index.html')
+    cy.contains('button', 'Login').click()
+  })
+  
+  it("it disable the captcha verify button when no answer is provided or it´s cleared", () => {
+    cy.contains('button', 'Verify')
+      .should('be.disabled')
+      
+    cy.get('input[placeholder="Enter your answer"]')
+      .type('1')
+
+    cy.contains('button', 'Verify')
+      .should('be.enabled')
     
-  })
+    cy.get('input[placeholder="Enter your answer"]')
+      .clear()
 
-  it("captcha error", () => {
-    
+      cy.contains('button', 'Verify')
+      .should('be.disabled')
   })
+  
+  it("it shows an error on wrong captcha answer and goes back to its initial state", () => {
+    cy.get('input[placeholder="Enter your answer"]')
+      .type('1000')
 
-  it("run button - enabled/disabled states", () => {
-    
-  })
+    cy.contains('button', 'Verify')
+      .click()
+      
+    cy.contains('.error', 'Incorrect answer, please try again.')
+      .should('be.visible')
 
-  it("reset textarea on logout and login", () => {
-    
-  })
-
-  it("disabled run button on logout and login", () => {
-   
-  })
-
-  it("reset output on logout and login", () => {
-    
-  })
-
-  it("disabled run button on logout and login", () => {
-    
+    cy.get('input[placeholder="Enter your answer"]')
+      .should('have.value', '')
+    cy.contains('button', 'Verify').should('be.disabled') 
   })
 })
